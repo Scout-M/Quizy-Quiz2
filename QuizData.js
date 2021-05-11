85d4eea40de404c8456313fc984a9a018d4ee710
Amy <74619224+Scout-M@users.noreply.github.com>
2021-05-11 09:15:07 -0400

    Add files via upload

---
+++ QuizData.js
@@ -0,0 +1,32 @@
+export const QuizData = [
+  {
+    id: 0,
+    question: `Which is most likely to kill you?`,
+    options: [`cow`, `cat`, `shark`, `bird`],
+    answer: `Cow`,
+  },
+  {
+    id: 1,
+    question: `Which land mammal has the biggest eyes?`,
+    options: [`Horse`, `Camel`, `Elephant`, `Cow`],
+    answer: `Horse`,
+  },
+  {
+    id: 2,
+    question: `What is the largest species of shark?`,
+    options: [`Hammerhead`, `Great White`, `Whale Shark`, `Tiger Shark`],
+    answer: `Whale Shark`,
+  },
+  {
+    id: 3,
+    question: `How long does the Giant Pacific Octopus live`,
+    options: [`10 years`, `5 years`, `2 years`, `1 year`],
+    answer: `1 year`,
+  },
+  {
+    id: 4,
+    question: `Which animal has the most fur?`,
+    options: [`fox`, `Sea otter`, `Dog`, `Polar Bear`],
+    answer: `Sea otter`,
+  },
+];
