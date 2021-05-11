5d09fed148fae54ec750b803edbff3f1c703de6d
Amy <74619224+Scout-M@users.noreply.github.com>
2021-05-11 09:14:51 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import Quiz from './components/Quiz'
+
+function App() {
+  return (
+  
+  <div>
+
+  <Quiz/>
+  </div>
+  )
+}
+
+export default App
+
+ReactDOM.render(<App />, document.getElementById("root"))
